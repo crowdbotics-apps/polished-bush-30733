1aa819a28e3d88cc46d474e430b897b21abc82bf
Crowdbotics <team@crowdbotics.com>
2021-09-22 01:19:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "polished_bush_30733Identifier",
+  url: "https://polished_bush_30733Identifier.botics.co",
+  api: "https://polished_bush_30733Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
